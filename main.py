e0b0533b1a0fefc1daa9873500f6a2a1afd9d4fd
Judah B <ConvexPolyhedron@Outlook.com>
2021-07-08 16:48:10 +0000

    Add IPFS hashes in, add a new API point for getting the access URL.

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template, request, session, redirect, url_for
+from flask import Flask, render_template, request, session, redirect, url_for, jsonify
 # import os
 app = Flask('app')
 project_list=[]
 # session_secret = os.environ['session-secret']
 class project:
-  def __init__(self, creator, name="Untitled Project", desciption="",license="CC0"):
+  def __init__(self, creator, hash, name="Untitled Project", desciption="",license="CC0"):
     self.name=name
     self.description=desciption
     self.creator=creator
@@ -13,6 +13,8 @@ class project:
       self.license="CC0"
     else:
       self.license=license
+    self.hash=hash
+    self.sourceurl=f"https://dweb.link/ipfs/{hash}"
 
 
 @app.route('/')
@@ -21,7 +23,7 @@ def home():
   
 @app.route('/project/<int:project_number>/')
 def project_page(project_number):
-  if project_number in project_list:
+  if 0 <= project_number < len(project_list):
     return render_template('project.html', project_number=project_number)
   else:
         return render_template('project-404.html', project_number=project_number)
@@ -29,7 +31,7 @@ def project_page(project_number):
 @app.route('/create/', methods=['GET', 'POST'])
 def createproject_page():
   if request.method == 'POST':
-    newProject = project("system", name = request.form["name"], license="CC0", desciption=request.form["description"])
+    newProject = project("system", request.form['hash'], name = request.form["name"], license="CC0", desciption=request.form["description"])
     project_list.append(newProject)
     return redirect(url_for('project_page', project_number=project_list.index(newProject)))
   return '''
@@ -37,8 +39,18 @@ def createproject_page():
             <input type=text name=name placeholder="Project Name"><br>
             <input type=text name=description placeholder="Description">
             <br>
+            <input type=text name=hash placeholder="IPFS hash">
+            <br>
             <input type=submit value=Create>
         </form>
     '''
-
+@app.route("/api/")
+def api_root():
+  return jsonify(status="200")
+@app.route("/api/getprojectsource/<int:project_number>")
+def src_api(project_number):
+  if 0 <= project_number < len(project_list):
+    return jsonify(status="200",url=project_list[project_number].sourceurl)
+  else:
+    return jsonify(status="404",error="Project does not exist")
 app.run(host='0.0.0.0', port=8080)
