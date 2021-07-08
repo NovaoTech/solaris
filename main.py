0d1d0c7cda4bc7b9224c1824f8c4d46048dea3b7
Judah B <ConvexPolyhedron@Outlook.com>
2021-07-08 00:44:28 +0000

    Make basic structure for HTML/CSS

--- main.py
+++ main.py
@@ -4,4 +4,7 @@ app = Flask('app')
 @app.route('/')
 def home():
   return render_template('index.html')
+@app.route('/assets/<file>')
+def staticfile():
+  return url_for('static', filename=escape(file))
 app.run(host='0.0.0.0', port=8080)
