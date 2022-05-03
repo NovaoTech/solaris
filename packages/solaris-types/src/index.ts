068517449e96ae989de09d75407cb9b717f7a1cd
Judah Brown <hello@judahbrown.xyz>
2022-05-03 18:36:22 -0500

    feat(solaris-types): Add new 'solaris-types' module This commit adds a new solaris-types module for TypeScript support.

---
+++ packages/solaris-types/src/index.ts
@@ -0,0 +1,19 @@
+export interface Project {
+    config: Object,
+    assets: Array<any>
+}
+
+export interface File {
+    name: string,
+    bytes: string
+}
+
+export interface Asset {
+    hash: string,
+    bytes: string
+}
+
+export interface RequiredAsset {
+    name: string,
+    hash: string
+}
