7b3ba28e3bcf53671e7698ab64f591a19acd7ad6
Judah Brown <hello@judahbrown.xyz>
2022-05-04 17:56:20 -0500

    feat(dynamo): Add asset uploading
    Adds a new method for uploading assets via POST request.
    Assets can be POST'd to the /assets/ route with the bytes.
    If the asset already exists, the server will return the hash.
    If the asset does not exist, the server will upload it and return the hash.

--- packages/dynamo/src/index.ts
+++ packages/dynamo/src/index.ts
@@ -1,11 +1,13 @@
 // Import dependencies
 import express from 'express'
 import dotenv from 'dotenv'
+import {sha512} from 'sha512-crypt-ts'
 const {MongoClient, ServerApiVersion, ObjectId} = require('mongodb')
 
 // Import solaris packages
 import * as solaris from 'solaris-types'
 import * as sdb from './db'
+import {error} from 'console'
 
 // Import from .env
 dotenv.config()
@@ -28,7 +30,7 @@ if (process.env.DB) {
 } else {
   determinedDB = 'testing'
 }
-const database = determinedDB
+const database = 'testing'
 
 // Determine the port based on .env
 let determinedPort: number
@@ -39,16 +41,14 @@ if (process.env['DYNAMO_PORT']) {
 }
 const port: number = determinedPort
 
-// Use JSON Parsing.
-app.use(express.json())
-
 // Process Requests
+app.use(express.json())
 app.get('/', (_req, _res) => {
   _res.json({status: '200'})
 })
 
 app.get('/assets/:assetID', async (req, res) => {
-  let foundAsset = await Promise.resolve(sdb.find('testing', 'assets', {hash: req.params.assetID}, client))
+  let foundAsset = await Promise.resolve(sdb.find(database, 'assets', {hash: req.params.assetID}, client))
   if (foundAsset != null) {
     res.json(foundAsset)
   } else {
@@ -56,6 +56,33 @@ app.get('/assets/:assetID', async (req, res) => {
   }
 })
 
+app.get('/test/', async (req, res) => {
+  sdb.insert(database, 'test', {text: 'Yay, it works!', date: Date.now}, client)
+  res.status(200).json({status: '200'})
+})
+
+app.post('/assets/', async (req, res) => {
+  let body
+  try {
+    body = req.body
+  } catch {
+    res.status(400).json({status: '400'})
+  }
+
+  if (body.bytes) {
+    let hash: string = sha512.crypt(body.bytes, 'saltysalt').replace('$6$saltysalt$', '')
+    let foundAsset: Object = await Promise.resolve(sdb.find(database, 'assets', {hash: hash}, client))
+    if (foundAsset != null) {
+      console.log(foundAsset)
+      res.status(200).json({hash: hash})
+    } else {
+      sdb.insert(database, 'assets', {hash: hash, bytes: body.bytes}, client)
+      res.status(200).json({hash: hash})
+    }
+  } else {
+    res.status(400).json({status: '400'})
+  }
+})
 // Handle Errors
 app.use(function (req, res, next) {
   res.status(404).json({status: '404'})
